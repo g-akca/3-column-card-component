dc2beb097f8bfd31424efe8831aefbfb13c1c6a9
g-akca <gok.akca66@gmail.com>
2026-05-05 17:13:23 +0300

    Import icons, move color object outside Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,24 +1,28 @@
-function Card({ icon, title, description, primaryColor = "gold" }) {
-  const colorVariants = {
-    gold: {
-      bg: "bg-gold-500",
-      text: "text-gold-500"
-    },
-    cyan: {
-      bg: "bg-cyan-800",
-      text: "text-cyan-800"
-    },
-    green: {
-      bg: "bg-green-950",
-      text: "text-green-950"
-    }
+const colorVariants = {
+  gold: {
+    bg: "bg-gold-500",
+    text: "text-gold-500"
+  },
+  cyan: {
+    bg: "bg-cyan-800",
+    text: "text-cyan-800"
+  },
+  green: {
+    bg: "bg-green-950",
+    text: "text-green-950"
   }
+}
 
+function Card({ icon, title, description, primaryColor = "gold" }) {
   const colors = colorVariants[primaryColor] || colorVariants.gold;
 
   return (
-    <div className={`p-12 ${colors.bg}`}>
+    <div className={`p-12 ${colors.bg} flex flex-col items-start gap-8`}>
+      <img src={icon} alt={`${title} icon`} className="h-10" />
 
+      <div>
+
+      </div>
     </div>
   )
 }
--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -1,21 +1,25 @@
 import Card from "./Card";
 
+import sedansIcon from "/images/icon-sedans.svg";
+import suvsIcon from "/images/icon-suvs.svg";
+import luxuryIcon from "/images/icon-luxury.svg";
+
 function CardContainer() {
   const cards = [
     {
-      icon: "/images/icon-sedans.svg",
+      icon: sedansIcon,
       title: "Sedans",
       description: "Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.",
       primaryColor: "gold"
     },
     {
-      icon: "/images/icon-suvs.svg",
+      icon: suvsIcon,
       title: "SUVs",
       description: "Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures.",
       primaryColor: "cyan"
     },
     {
-      icon: "/images/icon-luxury.svg",
+      icon: luxuryIcon,
       title: "Luxury",
       description: "Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.",
       primaryColor: "green"
