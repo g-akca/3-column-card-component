93175e30752208ce14ba4508997ceb91c8807ac6
g-akca <gok.akca66@gmail.com>
2026-05-05 18:22:46 +0300

    Add tablet responsive Card styles

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -17,7 +17,7 @@ function Card({ icon, title, description, primaryColor = "gold" }) {
   const colors = colorVariants[primaryColor] || colorVariants.gold;
 
   return (
-    <div className={`p-12 ${colors.bg} flex flex-col items-start gap-8`}>
+    <div className={`p-12 tablet:px-20 ${colors.bg} flex flex-col items-start gap-8`}>
       <img src={icon} alt={`${title} icon`} className="h-10" />
 
       <div className="flex flex-col gap-6 items-start">
