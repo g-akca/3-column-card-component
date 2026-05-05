856feadab5b1ffd103c9c49bb8f7a6f6d050e190
g-akca <gok.akca66@gmail.com>
2026-05-05 13:59:24 +0300

    Create Card component, add CardContainer styles

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -1,7 +1,12 @@
+import Card from "./Card";
+
 function CardContainer() {
   return (
-    <>
-    </>
+    <main className="w-full grid-rows-3 rounded-lg overflow-hidden">
+      <Card />
+      <Card />
+      <Card />
+    </main>
   )
 }
 
