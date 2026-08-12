6b6554cbc95096bd5ab6051276bb035057a3488c
g-akca <gok.akca66@gmail.com>
2026-08-12 19:15:57 +0300

    Fix alt attribute for icon image in Card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -18,7 +18,7 @@ function Card({ icon, title, description, primaryColor = "gold" }) {
 
   return (
     <div className={`p-12 tablet:px-20 desktop:px-12 ${colors.bg} flex flex-col items-start gap-8 desktop:gap-10`}>
-      <img src={icon} alt={`${title} icon`} className="h-10" />
+      <img src={icon} alt="" className="h-10" />
 
       <div className="grow flex flex-col gap-6 items-start desktop:justify-between">
         <div className="flex flex-col gap-6">
