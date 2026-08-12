2749e2d6aadadfbc9a6d7d1cdc6cabb4bf540bf2
g-akca <gok.akca66@gmail.com>
2026-08-12 19:16:49 +0300

    Add heading for accessibility in App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import CardContainer from "./components/CardContainer";
 function App() {
   return (
     <div className="text-base leading-base text-white min-h-screen bg-gray-100 px-6 py-12 tablet:px-20 tablet:py-22 flex justify-center items-center">
+      <h1 className="sr-only">Car rental options</h1>
+      
       <CardContainer />
     </div>
   )
